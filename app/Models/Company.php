3e2f92899fc659c0bf5373368eaa39b450582f6d
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-09-18 19:17:48 +0200

    wip

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -11,10 +11,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\Image\Enums\Fit;
 
 class Company extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia, Sluggable,SoftDeletes;
+    use HasFactory, InteractsWithMedia, Sluggable, SoftDeletes;
 
     public $table = 'companies';
 
@@ -48,8 +49,8 @@ class Company extends Model implements HasMedia
 
     public function registerMediaConversions(?Media $media = null): void
     {
-        $this->addMediaConversion('thumb')->fit('crop', 50, 50);
-        $this->addMediaConversion('preview')->fit('crop', 120, 120);
+        $this->addMediaConversion('thumb')->fit(Fit::Contain, 50, 50);
+        $this->addMediaConversion('preview')->fit(Fit::Contain, 'crop', 120, 120);
     }
 
     public function getLogoAttribute()
